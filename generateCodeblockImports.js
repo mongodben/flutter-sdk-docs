420547cc47080103f121a6648b77cab4f91b81c4
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-02-01 16:05:19 -0500

    demo site

---
+++ generateCodeblockImports.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const importsArr = [];
+fs.readdirSync(path.join(__dirname, "snippets")).forEach((snippet) => {
+  console.log(snippet);
+  const snippetComponentName = snippet
+    .split(/\.|-|_/)
+    .map((segment) => {
+      return segment[0].toUpperCase() + segment.substring(1);
+    })
+    .join("");
+  const importStatement = `import ${snippetComponentName} from '!!raw-loader!./snippets/${snippet}';`;
+  importsArr.push(importStatement);
+});
+console.log(`
+
+import statements for the docs:
+
+`);
+console.log(importsArr.join("\n"));
